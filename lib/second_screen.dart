0192486f13c9ae279170c224958fd2cbf48e4ede
Frederik Dumazy <frederik.dumazy@duo.be>
2023-02-17 15:46:08 +0100

    Add animation

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -3,15 +3,44 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class SecondScreen extends StatelessWidget {
+class SecondScreen extends StatefulWidget {
   final WordPair pair;
   const SecondScreen({super.key, required this.pair});
 
   @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  Color color = Colors.blue;
+
+  void changeColor() {
+    setState(() {
+      color = Colors.red;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(pair.asString),
+    return WillPopScope(
+      onWillPop: () => Future.value(true),
+      child: Theme(
+        data: ThemeData(primarySwatch: Colors.green),
+        child: Scaffold(
+          appBar: AppBar(
+            foregroundColor: Colors.purple,
+          ),
+          body: Center(
+              child: GestureDetector(
+            onTap: changeColor,
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              color: color,
+              width: 200,
+              height: 200,
+            ),
+          )),
+        ),
       ),
     );
   }
